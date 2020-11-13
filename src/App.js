18c81a251ef9b17098c5929a4de84791884697e7
Sumesh Kodappora <sumesh@192-168-1-102.tpgi.com.au>
2020-11-13 16:23:06 +1100

    cognito userid and email as data with keyword"

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import config from './aws-exports';
 import { withAuthenticator, AmplifySignOut} from '@aws-amplify/ui-react';
 import './App.css';
 import axios from 'axios';
+import { Auth } from 'aws-amplify';
 
 const api = axios.create({
   baseURL:'https://r8y325hxrj.execute-api.ap-southeast-2.amazonaws.com/prod/streams/keywordteller/record'
@@ -12,10 +13,18 @@ const api = axios.create({
 Amplify.configure(config)
 
 function App() {
+  let id = null
+  let email = null
+
+  Auth.currentUserInfo().then(info => {
+    id = info.id
+    if(info.attributes) {email = info.attributes.email}
+  });
   const [searchWord, setSearchWord] = React.useState('')
   
   const sendSearchword =async ()=> {
-    let res =await api.put('/', {"Data": searchWord, "PartitionKey":"1"})
+    console.log(id)
+    let res =await api.put('/', {"Data": {"searchword" : searchWord , "id" : id, "email" :email}, "PartitionKey":"1"})
     console.log(res)
   }
   
