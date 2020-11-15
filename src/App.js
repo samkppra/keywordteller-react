17cc510ac42d812cc7dd6c0aeb36045e3261b749
Sumesh Kodappora <sumesh@192-168-1-102.tpgi.com.au>
2020-11-16 01:02:43 +1100

    api added for db poll

--- src/App.js
+++ src/App.js
@@ -10,29 +10,49 @@ const api = axios.create({
   baseURL:'https://r8y325hxrj.execute-api.ap-southeast-2.amazonaws.com/prod/streams/keywordteller/record'
 })
 
+const getApi = axios.create({
+  baseURL:'https://2cdaff1xc7.execute-api.ap-southeast-2.amazonaws.com/prod'
+})
+let id = null
+let email = null
+
 Amplify.configure(config)
 
 function App() {
-  let id = null
-  let email = null
-
+  
+  
   Auth.currentUserInfo().then(info => {
     id = info.id
     if(info.attributes) {email = info.attributes.email}
   });
+
   const [searchWord, setSearchWord] = React.useState('')
+  const [result, setResult] = React.useState([])
   
-  const sendSearchword =async ()=> {
-    console.log(id)
-    let res =await api.put('/', {"Data": {"Keyword" : searchWord , "CustomerId" : id, "Email" :email}, "PartitionKey":"1"})
-    console.log(res)
+ 
+  const sendSearchword = async () => {
+    const res= await api.put('/', {"Data": {"Keyword" : searchWord , "CustomerId" : id, "Email" :email}, "PartitionKey":"1"})
+  console.log(res)
+  console.log(searchWord)
+  }
+
+  const componentDidMount = async () => {
+    let suggestions =await getApi.get('/id/'+id)
+    console.log(suggestions)
+    const renderedGoods = suggestions.data.Items.map(item => {
+       return item.Data
+    })
+    console.log(renderedGoods)
+    setResult(renderedGoods)
   }
   
+
   const handleSubmit = e=> {
     e.preventDefault()
-    console.log(searchWord)
     sendSearchword()
+    componentDidMount()
   }
+   
   return (
     <div className="App">
       <header className="App-header">
@@ -40,6 +60,10 @@ function App() {
           <input value={searchWord} placeholder="search" onChange={(e) => setSearchWord(e.target.value)}></input>
           <button>Search</button>
         </form>
+         
+        
+       <ul>{result.map(i => <li>{JSON.stringify(i)}</li>)}</ul>
+        
         <AmplifySignOut></AmplifySignOut>
       </header>
     </div>
